114b3642ae3d733bc0047e0e0d1529affbf1a2bc
SirNotFunny <133789890+SirNotFunny@users.noreply.github.com>
2025-03-27 16:21:13 +0700

    Time: 0 ms (100%), Space: 12.4 MB (91.76%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -9,9 +9,9 @@ class Solution(object):
         for s in strs:
             if len(s)<min_length: #min len of the smallest word in string array
                 min_length = len(s)
-        while i < min_length:
+        while i < min_length: #i mean.. if there is a word with 3 letters in an array with 3+ word, the maximum amount of identical prefix is also 3...
             for s in strs:
                 if s[i] != strs[0][i]: #first character of the i-th word
                     return s[:i] # ":i" is "up until i"
-            i+=1
+            i+=1 #move to next word
         return s[:i]
